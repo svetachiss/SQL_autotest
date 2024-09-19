38ee5ccddd6ee25c84b7170e677957fccd5e237e
svetachiss <svetachiss7@gmail.com>
2024-09-19 21:43:01 +0300

    result

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://5e1a05d0-16f5-47a1-932b-0491acea680d.serverhub.praktikum-services.ru"
+URL_SERVICE = "https://a7f12ba7-6f3f-44ca-ab78-699c80a97cb8.serverhub.praktikum-services.ru"
 CREATE_ORDER_PATH = "/api/v1/orders"
 GET_TRACK_PATH = "/api/v1/orders//track?t="
